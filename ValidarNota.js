f9cf52867b61b64cc82ead79cd39ee5e8e94ac03
Carlos Guilherme <103211459+Guilherme-Dev-Java@users.noreply.github.com>
2025-05-06 12:28:24 -0300

    Feat: implementação da validação de nota
    Programa válida a nota em Aprovado, recuperação ou reprovado.

---
+++ ValidarNota.js
@@ -0,0 +1,20 @@
+/*Exercício 2 
+Programa classifica se em Aprovado, Recuperação e Reprovado a pratir de uma nota informada, 
+utilizando a estrutura de controle if-else */
+
+const prompt = require('prompt-sync')()
+
+let nota = Number(prompt('Digite a nota: '))
+
+if(nota < 0 || nota > 10){
+    console.log('Digite uma nota Válida')
+    return
+}
+
+if (nota >= 6 ){
+    console.log('Parabéns, Aprovado.')
+}else if(nota >= 4 && nota < 6){
+    console.log('Estude mais, você está em Recuperação.')
+}else{
+    console.log('Reprovado.')
+}
